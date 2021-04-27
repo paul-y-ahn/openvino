16e8bb22cba55426872841d1e677aef6669ceba9
Paul Y Ahn <paul.y.ahn@intel.com>
2021-04-27 22:04:44 +0900

    print perf result

--- inference-engine/tests/functional/shared_test_classes/src/base/layer_test_utils.cpp
+++ inference-engine/tests/functional/shared_test_classes/src/base/layer_test_utils.cpp
@@ -19,6 +19,7 @@
 #include "ngraph/variant.hpp"
 #include "shared_test_classes/base/layer_test_utils.hpp"
 #include "functional_test_utils/core_config.hpp"
+#include "cldnn/cldnn_config.hpp"
 
 namespace LayerTestsUtils {
 
@@ -211,6 +212,8 @@ void LayerTestsCommon::ConfigureNetwork() {
             out.second->setPrecision(outPrc);
         }
     }
+    core->SetConfig({{CONFIG_KEY(PERF_COUNT), CONFIG_VALUE(YES)}}, targetDevice);
+    core->SetConfig({{CLDNN_CONFIG_KEY(SOURCES_DUMPS_DIR), "/home/ahnyoung/cldnn/openvino/bin/intel64/Debug/exec_graph_dump/kernels"}}, targetDevice);
 }
 
 void LayerTestsCommon::LoadNetwork() {
@@ -234,6 +237,69 @@ void LayerTestsCommon::GenerateInputs() {
     }
 }
 
+static std::vector<std::pair<std::string, InferenceEngine::InferenceEngineProfileInfo>>
+perfCountersSorted(std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> perfMap) {
+    using perfItem = std::pair<std::string, InferenceEngine::InferenceEngineProfileInfo>;
+    std::vector<perfItem> sorted;
+    for (auto &kvp : perfMap) sorted.push_back(kvp);
+
+    std::stable_sort(sorted.begin(), sorted.end(),
+                     [](const perfItem& l, const perfItem& r) {
+                         return l.second.execution_index < r.second.execution_index;
+                     });
+
+    return sorted;
+}
+
+static void printPerformanceCounts(const std::map<std::string, InferenceEngine::InferenceEngineProfileInfo>& performanceMap,
+                                          std::ostream &stream, std::string deviceName,
+                                          bool bshowHeader = true) {
+    long long totalTime = 0;
+    // Print performance counts
+    if (bshowHeader) {
+        stream << std::endl << "performance counts:" << std::endl << std::endl;
+    }
+
+    auto performanceMapSorted = perfCountersSorted(performanceMap);
+    std::cout << "performanceMap : " << performanceMap.size() << std::endl;
+    std::cout << "performanceMapSorted : " << performanceMapSorted.size() << std::endl;
+
+    for (const auto & it : performanceMapSorted) {
+        std::string toPrint(it.first);
+        const int maxLayerName = 30;
+        // if (std::string(it.second.layer_type).find("ConvolutionBackprop") == std::string::npos) continue;
+        if (it.first.length() >= maxLayerName) {
+            toPrint  = it.first.substr(0, maxLayerName - 4);
+            toPrint += "...";
+        }
+
+        stream << std::setw(maxLayerName) << std::left << toPrint;
+        switch (it.second.status) {
+        case InferenceEngine::InferenceEngineProfileInfo::EXECUTED:
+            stream << std::setw(15) << std::left << "EXECUTED";
+            break;
+        case InferenceEngine::InferenceEngineProfileInfo::NOT_RUN:
+            stream << std::setw(15) << std::left << "NOT_RUN";
+            break;
+        case InferenceEngine::InferenceEngineProfileInfo::OPTIMIZED_OUT:
+            stream << std::setw(15) << std::left << "OPTIMIZED_OUT";
+            break;
+        }
+        stream << std::setw(30) << std::left << "layerType: " + std::string(it.second.layer_type) + " ";
+        stream << std::setw(20) << std::left << "realTime: " + std::to_string(it.second.realTime_uSec);
+        stream << std::setw(20) << std::left << "cpu: "  + std::to_string(it.second.cpu_uSec);
+        stream << " execType: " << it.second.exec_type << std::endl;
+        if (it.second.realTime_uSec > 0) {
+            totalTime += it.second.realTime_uSec;
+        }
+    }
+//    stream << std::setw(20) << std::left << "Total time: " + std::to_string(totalTime) << " microseconds" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Full device name: " << deviceName << std::endl;
+    std::cout << std::endl;
+}
+
+
 void LayerTestsCommon::Infer() {
     inferRequest = executableNetwork.CreateInferRequest();
 
@@ -254,6 +320,13 @@ void LayerTestsCommon::Infer() {
         inferRequest.SetBatch(batchSize);
     }
     inferRequest.Infer();
+
+    auto execGraphInfo = executableNetwork.GetExecGraphInfo();
+    std::string exec_graph_path = "/home/ahnyoung/cldnn/openvino/bin/intel64/Debug/exec_graph_dump/graph.xml";
+    execGraphInfo.serialize(exec_graph_path);
+    std::cout << "Now :: dumping exec graph to " << exec_graph_path << std::endl;
+    auto reqPerfCounts = inferRequest.GetPerformanceCounts();
+    printPerformanceCounts(reqPerfCounts, std::cout, "GPU", false);
 }
 
 std::vector<std::vector<std::uint8_t>> LayerTestsCommon::CalculateRefs() {
