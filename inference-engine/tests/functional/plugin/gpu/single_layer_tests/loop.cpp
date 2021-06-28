4dc95564696454bfb66d7f0d46bd9c8defae9a35
Eunsoo Sheen <eunsoo.sheen@intel.com>
2021-06-28 17:33:41 +0000

    Update functional test

--- inference-engine/tests/functional/plugin/gpu/single_layer_tests/loop.cpp
+++ inference-engine/tests/functional/plugin/gpu/single_layer_tests/loop.cpp
@@ -17,7 +17,14 @@ namespace {
             //  GCC4.8 limitation: have to specify type of each element in list
             //                               static_trip_count |  max | dynamic_exit | axis
             std::tuple<bool, int64_t, int64_t, int64_t>{  true ,  5, -1, -1 },  // n_iter 5, no dynamic exit
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{  true ,  5,  3, -1 },  // n_iter 3, dynamic exit on 3
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{  true ,  5,  7, -1 },  // n_iter 5, dynamic exit not reached
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{  true , -1,  5, -1 },  // n_iter 5, inf loop with dynamic exit on 5
             std::tuple<bool, int64_t, int64_t, int64_t>{  true ,  5, -1,  1 },  // n_iter 5, const for loop with auto concatenated out
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{ false ,  5, -1, -1 },  // |
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{ false ,  5,  3, -1 },  // | same with dynamic trip count
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{ false ,  5,  7, -1 },  // |
+        //     std::tuple<bool, int64_t, int64_t, int64_t>{ false , -1,  5, -1 }   // |
     };
 
     INSTANTIATE_TEST_CASE_P(smoke_StaticShapeLoop, StaticShapeLoopTest,
